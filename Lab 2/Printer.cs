5ac6011b71faa2bd331344f056a20bcfe2d04423
lm902 <i@lm902.cn>
2019-11-04 14:05:14 -0800

    Print assignment total

--- Lab 2/Printer.cs
+++ Lab 2/Printer.cs
@@ -16,7 +16,7 @@ namespace Lab_2
         }
 
         public static void PrintAssignmentGrades(Assignment assignment, IEnumerable<Grade> grades) {
-            var filteredGrades = from grade in grades where grade.Assignment == assignment select grade.Student.FullName + '\t' + grade.StudentTotal;
+            var filteredGrades = from grade in grades where grade.Assignment == assignment select grade.Student.FullName + '\t' + grade.StudentTotal + '/' + grade.Assignment.Total;
             Console.WriteLine("FullName\tStudentTotal");
             Console.WriteLine(string.Join("\n", filteredGrades));
         }
@@ -24,7 +24,7 @@ namespace Lab_2
         public static void PrintAssignmentStudentGrade(Grade grade)
         {
             Console.WriteLine("FullName(StudentID)\tAssignmentName\tStudentTotal");
-            Console.WriteLine(grade.Student.FullName + '(' + grade.Student.StudentID + ')' + '\t' + grade.Assignment.AssignmentName + '\t' + grade.StudentTotal);
+            Console.WriteLine(grade.Student.FullName + '(' + grade.Student.StudentID + ')' + '\t' + grade.Assignment.AssignmentName + '\t' + grade.StudentTotal + '/' + grade.Assignment.Total);
         }
     }
 }
