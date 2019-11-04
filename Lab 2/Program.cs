c5d36e4c3c6df25eb280074d3ffbd06594020ae0
lm902 <i@lm902.cn>
2019-11-04 14:03:48 -0800

    Add 3 grades

--- Lab 2/Program.cs
+++ Lab 2/Program.cs
@@ -12,11 +12,19 @@ namespace Lab_2
         {
             var section = new Section { SectionName = "Section 1", SectionNumber = "1" };
             var philWeier = new Student { FirstName = "Phil", LastName = "Weier", StudentID = "A00948735" };
+            var philWeier2 = new Student { FirstName = "Phil", LastName = "Weier", StudentID = "A00948735" };
+            var philWeier3 = new Student { FirstName = "Phil", LastName = "Weier", StudentID = "A00948735" };
             section.Students.Add(philWeier);
+            section.Students.Add(philWeier2);
+            section.Students.Add(philWeier3);
             var assignment = new Assignment { AssignmentName = "inClass2_cs Lab", Section = section, Total = 100 };
             var philsGrade = new Grade { Assignment = assignment, Student = philWeier, StudentTotal = 5 };
+            var philsGrade2 = new Grade { Assignment = assignment, Student = philWeier2, StudentTotal = 5 };
+            var philsGrade3 = new Grade { Assignment = assignment, Student = philWeier3, StudentTotal = 5 };
             var grades = new List<Grade>();
             grades.Add(philsGrade);
+            grades.Add(philsGrade2);
+            grades.Add(philsGrade3);
             Console.WriteLine("Test PrintStudentNames");
             Printer.PrintStudentNames(section);
             Console.WriteLine("\nTest PrintAssignmentGrades");
