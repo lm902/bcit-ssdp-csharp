ab0601993960bf77dd20ebd47371851ca75bae84
lm902 <i@lm902.cn>
2019-11-15 10:27:15 -0800

    Finish prototype

--- Zoo Meal Planner/Program.cs
+++ Zoo Meal Planner/Program.cs
@@ -11,6 +11,26 @@ namespace Zoo_Meal_Planner
     {
         static void Main(string[] args)
         {
+            var mammal = (Mammal)new Menu().RequestUserSelection();
+            decimal weight;
+            while (true)
+            {
+                Console.Write("Weight in KG: ");
+                var input = Console.ReadLine();
+                if(decimal.TryParse(input, out weight))
+                {
+                    break;
+                }
+            }
+            Console.WriteLine("Meal Recommendation");
+            Console.WriteLine("————————————————————");
+            Console.WriteLine("Mammal Type: %s", mammal.GetType().BaseType.Name);
+            Console.WriteLine("Species: %s", mammal.GetType().Name);
+            Console.WriteLine("Weight: %s", weight + " KG");
+            Console.WriteLine("Serving: %s", mammal.ServingWeightRatio * weight + " KG " + string.Join(", ", mammal.FoodPreference));
+            Console.WriteLine("Instructions:\r\n\tKeep area secure at all times.\r\n\tFeed at %s.", mammal.FeedTime);
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
         }
     }
 }
